28717ca746a1985002c92607248a23e59c5f2193
Ale Roses <ale.vrs@outlook.com>
2025-10-03 16:16:39 -0500

    23.5 Configuring Express

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Hi world!!");
-console.log("Other message!!");
+// const express = require("express");
+import express from "express";
+
+// Create the Express server
+const app = express();
+
+// Rutes
+app.get("/", (req, res) => {
+  console.log("The / is required.");
+
+  res.json({
+    ok: true,
+  });
+});
+
+// Listen to requests
+app.listen(4000, () => {
+  console.log("Server running on port");
+});
