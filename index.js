ae2805035e0f4061f6c64c133cdae71d5491366e
Ale Roses <ale.vrs@outlook.com>
2025-10-07 07:51:27 -0500

    23.13 Connect Node to Mongo Atlas

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import { router as authRoutes } from "./routes/auth.js";
+import { dbConnection } from "./database/config.js";
 
 // Create the Express server
 const app = express();
+
+// Data Base
+dbConnection();
 
 // Public directory
 app.use(express.static("public"));
