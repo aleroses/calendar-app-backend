67204aec6f793e11bf117ce62c8b9cd0ef5b4df4
Ale Roses <ale.vrs@outlook.com>
2025-10-10 08:53:18 -0500

    23.9 Update an Event

--- controllers/events.js
+++ controllers/events.js
@@ -35,11 +35,50 @@ export const createEvent = async (req, res = response) => {
   }
 };
 
-export const updateEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "Update event",
-  });
+export const updateEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      res.status(404).json({
+        ok: false,
+        msg: "The event doesn't exist for that ID",
+      });
+    }
+
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "You don't have privileges to edit this event.",
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid,
+    };
+
+    const updatedEvent = await Event.findByIdAndUpdate(
+      eventId,
+      newEvent,
+      { new: true }
+    );
+
+    res.json({
+      ok: true,
+      event: updatedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Talk to the administrator.",
+    });
+  }
 };
 
 export const deleteEvent = (req, res = response) => {
